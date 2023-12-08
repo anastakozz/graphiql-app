799b62721b7b7d5dbc064484eb4b35f598101343
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-08 17:31:12 +0300

    feat: integrate localization to welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,20 +1,45 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import userContext from '../lib/context';
+import { LocalizationData } from '../lib/interface';
+
+const getJSON = async (language: string, page: string) => {
+  try {
+    const res = await import(`../localization/${language}.json`);
+    return res[`${page}`];
+  } catch (error) {
+    console.error('Error loading JSON file:', error);
+  }
+};
 
 export default function WelcomePage() {
-  const { isUserLoggedIn } = useContext(userContext);
+  const { isUserLoggedIn, language } = useContext(userContext);
+  const [data, setData] = useState<LocalizationData | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getJSON(language, 'welcomePage');
+      console.log(data);
+      setData(data);
+    };
+
+    getData();
+  }, [language]);
 
   return (
     <>
-      <h1>Welcome!</h1>
-      {!isUserLoggedIn ? (
+      {data && (
         <>
-          <Link to="sign-in">sign in </Link>
-          <Link to="sign-up"> sign up</Link>{' '}
+          <h1>{data.title}</h1>
+          {!isUserLoggedIn ? (
+            <>
+              <Link to="sign-in">{data.signIn}</Link>
+              <Link to="sign-up">{data.signUp}</Link>
+            </>
+          ) : (
+            <Link to="main">{data.toMainPage}</Link>
+          )}
         </>
-      ) : (
-        <Link to="main">to main Page</Link>
       )}
     </>
   );
