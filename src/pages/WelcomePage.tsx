2a7124dbce7f82a5a58251a85937f4b3eac94b98
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-09 19:19:21 +0300

    feat: save language data in context

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,22 +1,18 @@
 import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import userContext from '../lib/context';
-import { LocalizationData } from '../lib/interface';
-import { getJSON } from '../lib/utils';
+import { pageData } from '../lib/interface';
 
 export default function WelcomePage() {
-  const { isUserLoggedIn, language } = useContext(userContext);
-  const [data, setData] = useState<LocalizationData | null>(null);
+  const { isUserLoggedIn, localData } = useContext(userContext);
+  const [data, setData] = useState<pageData | null>(null);
 
   useEffect(() => {
-    const getData = async () => {
-      const data = await getJSON(language, 'welcomePage');
-      console.log(data);
+    if (localData) {
+      const data = localData['welcomePage'];
       setData(data);
-    };
-
-    getData();
-  }, [language]);
+    }
+  }, [localData]);
 
   return (
     <>
