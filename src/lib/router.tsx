b6e6e27efe7027e5f1c3c094b750d9c903589e00
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-09 18:32:18 +0300

    fix: move notfound page out of layout

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -5,20 +5,22 @@ import Layout from '../components/Layout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<WelcomePage />} />
-      <Route path="sign-in" element={<SignInPage />} />
-      <Route path="sign-up" element={<SignUpPage />} />
-      <Route
-        path="main"
-        element={
-          <ProtectedRoute>
-            <MainPage />
-          </ProtectedRoute>
-        }
-      />
+    <>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="sign-in" element={<SignInPage />} />
+        <Route path="sign-up" element={<SignUpPage />} />
+        <Route
+          path="main"
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
-    </Route>
+    </>
   )
 );
 
