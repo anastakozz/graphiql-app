70a02a4f44d4aff27108f1be9a2834ee69b3297d
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-13 14:02:26 +0300

    refactor: replace Navigate with useNavigate

--- src/components/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.tsx
@@ -1,16 +1,25 @@
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FC, useEffect, useState } from 'react';
 import { setAuthListener } from '../../lib';
 import { Props } from './protectedRoutes.types';
 
 export const ProtectedRoute: FC<Props> = ({ children, isAuthPath }) => {
+  const navigate = useNavigate();
   const [hasCheckedAuth, setHasCheckedAuth] = useState(false);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   useEffect(() => setAuthListener({ setIsUserLoggedIn, setHasCheckedAuth }), []);
+
   if (!hasCheckedAuth) return null;
 
   if (isAuthPath) {
-    return isUserLoggedIn ? <Navigate to="/main" replace /> : <>{children}</>;
+    if (isUserLoggedIn) {
+      navigate('/main');
+    }
+    return <>{children}</>;
   }
-  return isUserLoggedIn ? <>{children}</> : <Navigate to="/" replace />;
+
+  if (isUserLoggedIn) {
+    return <>{children}</>;
+  }
+  navigate('/');
 };
