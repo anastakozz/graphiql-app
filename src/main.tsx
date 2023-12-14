10bce60403537a329166e3b9ec4bf29b7635bc69
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-15 00:20:36 +0300

    feat: add App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import './sass/main.scss';
-import { RouterProvider } from 'react-router-dom';
-import ErrorBoundary from './components/ErrorBoundary.tsx';
-import { ErrorPage } from './pages';
-import router from './lib/router.tsx';
-import { Provider } from 'react-redux';
-import store from './store/index.ts';
+import App from './ App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary fallback={<ErrorPage />}>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </ErrorBoundary>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
