2eb4943b419ad7a068ca6ca685399e0d5247c329
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-21 22:58:37 +0300

    feat: add query highlighting

--- src/components/JsonEditor/JsonEditor.tsx
+++ src/components/JsonEditor/JsonEditor.tsx
@@ -3,6 +3,7 @@ import CodeMirror from '@uiw/react-codemirror';
 import { jsonLanguage } from '@codemirror/lang-json';
 import { materialLightInit } from '@uiw/codemirror-themes-all';
 import { settingsCodemirror } from '../../lib/constants';
+import { graphql } from 'cm6-graphql';
 
 type Props = {
   viewMode: boolean;
@@ -30,12 +31,13 @@ export default function JsonEditor({ viewMode = false, value, onChange }: Partia
   return (
     <CodeMirror
       theme={materialLightInit({
-        settings: { foreground: '#6182B8', ...settingsCodemirror },
+        settings: settingsCodemirror,
       })}
       value={value}
       readOnly={false}
       className="json-input"
       onChange={(value) => onChange && onChange(value)}
+      extensions={[graphql()]}
     />
   );
 }
