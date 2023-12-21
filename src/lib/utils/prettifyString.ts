f50cb717997006a3528124eab1e60456d2c5044e
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-21 17:39:26 +0300

    feat: add keywords handling

--- src/lib/utils/prettifyString.ts
+++ src/lib/utils/prettifyString.ts
@@ -1,3 +1,26 @@
+export function checkKeyWords(input: string): string {
+  const keywords = [
+    'query',
+    'mutation',
+    'subscription',
+    'fragment',
+    'schema',
+    'type',
+    'input',
+    'enum',
+    'interface',
+    'union',
+  ];
+  let formattedString = input;
+
+  keywords.forEach((keyword) => {
+    const regex = new RegExp(`(${keyword})(?!\\s)`, 'g');
+    formattedString = formattedString.replace(regex, `$1 `);
+  });
+
+  return formattedString;
+}
+
 export function prettifyString(input: string): string {
   const jsonString = input.replace(/\s/g, '');
   let formattedJson = '';
@@ -29,7 +52,11 @@ export function prettifyString(input: string): string {
     }
 
     formattedJson += char;
+
+    if (char === '}' && jsonString[i + 1] !== '}') {
+      formattedJson += '\n' + '  '.repeat(indentationLevel);
+    }
   }
 
-  return formattedJson.trim();
+  return checkKeyWords(formattedJson.trim());
 }
