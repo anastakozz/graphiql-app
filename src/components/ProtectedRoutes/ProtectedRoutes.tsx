f0790fb72bca24d41a4aa21f18acfb509e556d73
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-23 21:59:02 +0300

    refactor: edit useEffect order

--- src/components/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.tsx
@@ -6,6 +6,7 @@ import { Props } from './protectedRoutes.types';
 export const ProtectedRoute: FC<Props> = ({ children, isAuthPath }) => {
   const [hasCheckedAuth, setHasCheckedAuth] = useState(false);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
   useEffect(() => setAuthListener({ setIsUserLoggedIn, setHasCheckedAuth }), []);
 
   if (!hasCheckedAuth) return null;
@@ -13,5 +14,6 @@ export const ProtectedRoute: FC<Props> = ({ children, isAuthPath }) => {
   if (isAuthPath) {
     return isUserLoggedIn ? <Navigate to="/main" replace /> : <>{children}</>;
   }
+
   return isUserLoggedIn ? <>{children}</> : <Navigate to="/" replace />;
 };
