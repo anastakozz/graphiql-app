f0969991b8b93caccaafc38c299a6e70ac4b01e0
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-23 15:07:23 +0300

    fix: fix router to make 404 page local

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -5,36 +5,34 @@ import Layout from '../components/Layout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<WelcomePage />} />
-        <Route
-          path="sign-in"
-          element={
-            <ProtectedRoute isAuthPath={true}>
-              <SignInPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="sign-up"
-          element={
-            <ProtectedRoute isAuthPath={true}>
-              <SignUpPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="main"
-          element={
-            <ProtectedRoute isAuthPath={false}>
-              <MainPage />
-            </ProtectedRoute>
-          }
-        />
-      </Route>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<WelcomePage />} />
+      <Route
+        path="sign-in"
+        element={
+          <ProtectedRoute isAuthPath={true}>
+            <SignInPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="sign-up"
+        element={
+          <ProtectedRoute isAuthPath={true}>
+            <SignUpPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="main"
+        element={
+          <ProtectedRoute isAuthPath={false}>
+            <MainPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<NotFoundPage />} />
-    </>
+    </Route>
   )
 );
 
