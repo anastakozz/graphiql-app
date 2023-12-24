7f253e0c106cb114e9effa2be0a85bf732a93b9f
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-24 22:02:35 +0300

    feat: add loading flag to UrlInput

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,6 @@
+import DoneIcon from './done-icon';
+import EnterIcon from './enter-icon';
+import OpenIcon from './open-icon';
+import UpdateIcon from './update-icon';
+
+export { DoneIcon, EnterIcon, OpenIcon, UpdateIcon };
