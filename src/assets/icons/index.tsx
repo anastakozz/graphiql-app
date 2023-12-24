e44e51f5d3fa77cd4f67b5b5608a0191bb1a3ce9
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-24 22:13:27 +0300

    fix: fix import paths

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,6 +1,5 @@
 import DoneIcon from './done-icon';
-import EnterIcon from './enter-icon';
 import OpenIcon from './open-icon';
 import UpdateIcon from './update-icon';
 
-export { DoneIcon, EnterIcon, OpenIcon, UpdateIcon };
+export { DoneIcon, OpenIcon, UpdateIcon };
