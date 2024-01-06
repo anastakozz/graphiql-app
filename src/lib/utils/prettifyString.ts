2442cc57008428412f9bd962b68968099927b6fa
anastakozz <117598324+anastakozz@users.noreply.github.com>
2024-01-06 14:53:31 +0300

    fix: remove line breaks inside brackets and add between siblings

--- src/lib/utils/prettifyString.ts
+++ src/lib/utils/prettifyString.ts
@@ -1,4 +1,4 @@
-export function checkKeyWords(input: string): string {
+function checkKeyWords(input: string): string {
   const keywords = [
     'query',
     'mutation',
@@ -22,6 +22,38 @@ export function checkKeyWords(input: string): string {
   });
 
   return formattedString;
+}
+
+function checkBrackets(input: string): string {
+  let result = '';
+  let insideParentheses = false;
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input.charAt(i);
+    const nextChar = input[i + 1];
+    const prevChar = result[result.length - 1];
+
+    if (char === '(') {
+      insideParentheses = true;
+    } else if (char === ')') {
+      insideParentheses = false;
+    }
+
+    if (insideParentheses) {
+      if (char === '\n') {
+        continue;
+      } else if (
+        (char === ' ' && (nextChar === ' ' || nextChar === ']')) ||
+        (prevChar === ' ' && (char === ' ' || char === '['))
+      ) {
+        continue;
+      }
+    }
+
+    result += char;
+  }
+
+  return result;
 }
 
 export function prettifyString(jsonString: string, isQuery = false): string {
@@ -50,11 +82,17 @@ export function prettifyString(jsonString: string, isQuery = false): string {
     } else if (char === ':') {
       formattedJson += ': ';
       continue;
-    } else if (char === ',') {
+    } else if (char === ',' && !symbols.test(prevChar)) {
       formattedJson += ',' + '\n' + '  '.repeat(indentationLevel);
       continue;
     }
-    if (isQuery && char === ' ' && symbols.test(prevChar) && symbols.test(nextChar)) {
+
+    if (
+      isQuery &&
+      char === ' ' &&
+      (symbols.test(prevChar) || prevChar === ('}' || ']')) &&
+      symbols.test(nextChar)
+    ) {
       formattedJson += '\n' + '  '.repeat(indentationLevel);
       continue;
     }
@@ -62,5 +100,7 @@ export function prettifyString(jsonString: string, isQuery = false): string {
     formattedJson += char;
   }
 
-  return isQuery ? checkKeyWords(formattedJson.trim()) : formattedJson.trim();
+  const result = isQuery ? checkKeyWords(formattedJson.trim()) : formattedJson.trim();
+
+  return checkBrackets(result);
 }
